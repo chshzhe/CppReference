ec12b3eef69b0e1930a9e5f97a6347bede7153a7
Chen Shuangzhe <chshzhe@126.com>
2021-12-01 22:09:55 +0800

    Add: Successfully AC T7

--- 7-Structure/07.cpp
+++ 7-Structure/07.cpp
@@ -2,47 +2,61 @@
 
 using namespace std;
 
-struct ListNode {
+struct ListNode
+{
     int val;
     ListNode *next;
 };
 
-
-ListNode* reverseList(ListNode* head) {
-    /********* Begin *************/
-
-
-
-
-    /********* End *************/
+ListNode *reverseList(ListNode *head)
+{
+    ListNode *pNode = head;
+    ListNode *pPrev = NULL;
+    ListNode *pNext = NULL;
+    ListNode *reverseHead = NULL;
+    while (pNode != NULL)
+    {
+        pNext = pNode->next;
+        reverseHead = pNode;
+        reverseHead->next = pPrev;
+        pPrev = pNode;
+        pNode = pNext;
+    }
+    return reverseHead;
 }
 
-
-
-ListNode* createList() {
+ListNode *createList()
+{
     int x;
-    ListNode head; 
-    ListNode* p = &head; head.next = NULL;
-    while (cin >> x) {
-        p->next = new ListNode; p->next->val = x; p->next->next = NULL;
-        p = p->next; 
+    ListNode head;
+    ListNode *p = &head;
+    head.next = NULL;
+    while (cin >> x)
+    {
+        p->next = new ListNode;
+        p->next->val = x;
+        p->next->next = NULL;
+        p = p->next;
     }
     return head.next;
 }
 
-void disp(ListNode* p) {
+void disp(ListNode *p)
+{
     while (p)
     {
-        cout<<p->val<<" -> ";
+        cout << p->val << " -> ";
         p = p->next;
     }
-    cout<<"nullptr\n";
+    cout << "nullptr\n";
 }
 
 int main()
 {
-    ListNode* L1 = createList(); disp(L1);
-    ListNode* L2 = reverseList(L1); disp(L2);
+    ListNode *L1 = createList();
+    disp(L1);
+    ListNode *L2 = reverseList(L1);
+    disp(L2);
 
     return 0;
 }
